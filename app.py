4fc3f45195c67e8f01bf862727463d17e75e23a8
Sifat <sifatk69@gmail.com>
2024-05-22 22:16:01 +0600

    second update

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+from transformers import pipeline
+from pathlib import Path
+import streamlit as st
+from transformers import LEDForConditionalGeneration, AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
 
 
-if __name__ == '__main__':
-    app.run()
+def _sanitize_path(self, relative_path):
+    print(f"Relative Path: {relative_path}")  # <-- Add this line
+    return Path(relative_path).resolve()
+
+
+st.title("Summarize Text")
+sentence = st.text_area('Please paste your article :', height=30)
+button = st.button("Summarize")
+
+with st.spinner("Generating Summary.."):
+    if button and sentence:
+        summarizer = pipeline("summarization", model="./model/falconsai")
+
+        summ=summarizer(sentence,max_length=2000, min_length=100, do_sample=False)
+
+        st.write(summ)
